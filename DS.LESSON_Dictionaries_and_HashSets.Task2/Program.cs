cdcbe109b2c206c3f17523eb2e3b7bf4e5bbccc8
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-31 10:37:52 +0200

    Добавьте файлы проекта.

---
+++ DS.LESSON_Dictionaries_and_HashSets.Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DS.LESSON_Dictionaries_and_HashSets.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "4,2,2,5,2,3,2,3,1,5,2,6,6,6";
+            string[] list = str.Split(",");
+            int[] count = new int[15];
+            int a = 0;
+            while (a < list.Length)
+            {
+                int b = Convert.ToInt32(list[a]);
+                count[b]++;
+                a++;
+            }
+            a = 0;
+            while (a < count.Length)
+            {
+                int b = Convert.ToInt32(count[a]);
+                if (((b % 2) != 0))
+                {
+                    int i = 0;
+                    while(i < list.Length)
+                    {
+                        if (Convert.ToInt32(list[i]) == a)
+                        {
+                            list[i] = null;
+                        }
+                        i++;
+                    }
+                }
+                a++;
+            }
+            a = 0;
+            while(a < list.Length)
+            {
+                Console.Write(list[a]);
+                a++;
+            }
+        }
+    }
+}
